f7ea735c52d8bba8edafdb1b0421a671acf6fd09
palascinochristian <palascinochristian@gmail.com>
2025-04-18 13:16:49 +0200

    Milestone 2

---
+++ functions.php
@@ -0,0 +1,17 @@
+<?php
+
+// Definisco una funzione per generare la password a cui passo la lunghezza desiderata come argomento
+    function generatePassword($length = 0) {
+        // Definisco i caratteri che possono essere usati per generare la password inclusi caratteri speciali e numeri
+        $characters = '0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ!@#$%^&*()_+[]{}|;:,.<>?';
+        $password = ''; // Inizializzo la password come stringa vuota
+        // Genero la password randomicamente con un ciclo for
+        for ($i = 0; $i < $length; $i++) {
+            // Seleziono un carattere casuale dalla stringa di caratteri
+            // e lo concateno alla password
+            $password .= $characters[rand(0, strlen($characters) - 1)];
+        }
+        return $password;
+    }
+
+?>
